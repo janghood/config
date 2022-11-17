3689992bef7ca1e0b881099bb9e2ff587df04cfd
你好怪凡 <higuaifan@higuaifan.com>
2022-11-18 02:31:39 +0800

    🔧 [config] 修复validateDocumentConfig中类型错误问题

--- lib/config.ts
+++ lib/config.ts
@@ -15,7 +15,7 @@ import os from 'os';
 import { build } from 'esbuild';
 import { pathToFileURL } from 'node:url';
 import { createRequire } from 'node:module';
-import { Documents, JanghoodConfig } from '../types/config';
+import { ApiExtractorType, Documents, JanghoodConfig } from '../types/config';
 
 
 export function lookupFile(
@@ -24,32 +24,32 @@ export function lookupFile(
   pathOnly = false
 ): string | undefined {
   for (const format of formats) {
-    const fullPath = path.join(dir, format)
+    const fullPath = path.join(dir, format);
     if (fs.existsSync(fullPath) && fs.statSync(fullPath).isFile()) {
-      return pathOnly ? fullPath : fs.readFileSync(fullPath, 'utf-8')
+      return pathOnly ? fullPath : fs.readFileSync(fullPath, 'utf-8');
     }
   }
-  const parentDir = path.dirname(dir)
+  const parentDir = path.dirname(dir);
   if (parentDir !== dir) {
-    return lookupFile(parentDir, formats, pathOnly)
+    return lookupFile(parentDir, formats, pathOnly);
   }
 }
 
 export const isWindows = os.platform() === 'win32';
 
 export function slash(p: string): string {
-  return p.replace(/\\/g, '/')
+  return p.replace(/\\/g, '/');
 }
 
 export function normalizePath(id: string): string {
-  return path.posix.normalize(isWindows ? slash(id) : id)
+  return path.posix.normalize(isWindows ? slash(id) : id);
 }
 
 export function isObject(value: unknown): value is Record<string, any> {
-  return Object.prototype.toString.call(value) === '[object Object]'
+  return Object.prototype.toString.call(value) === '[object Object]';
 }
 
-export const validateDocumentConfig = (config: JanghoodConfig, documentName: string) => {
+export const validateDocumentConfig = (config: JanghoodConfig & { apiExtractor?: ApiExtractorType }, documentName: string) => {
   if (!config.apiExtractor?.document ||
     !documentName ||
     !config.apiExtractor.document[documentName as keyof Documents]
@@ -58,7 +58,7 @@ export const validateDocumentConfig = (config: JanghoodConfig, documentName: str
     return false;
   }
   return config.apiExtractor.document[documentName as keyof Documents]!.active;
-}
+};
 
 export const DEFAULT_CONFIG_FILES = [
   'janghood.config.js',
@@ -67,74 +67,74 @@ export const DEFAULT_CONFIG_FILES = [
   'janghood.config.cjs',
   'janghood.config.mts',
   'janghood.config.cts'
-]
+];
 
 export async function loadJanghoodConfig(
   configFile?: string,
-  configRoot: string = process.cwd(),
+  configRoot: string = process.cwd()
 ): Promise<{
   path: string
   config: JanghoodConfig
   dependencies: string[]
 } | null> {
-  const start = performance.now()
-  const getTime = () => `${(performance.now() - start).toFixed(2)}ms`
+  const start = performance.now();
+  const getTime = () => `${(performance.now() - start).toFixed(2)}ms`;
 
-  let resolvedPath: string | undefined
+  let resolvedPath: string | undefined;
 
   if (configFile) {
     // explicit config path is always resolved from cwd
-    resolvedPath = path.resolve(configFile)
+    resolvedPath = path.resolve(configFile);
   } else {
     // implicit config file loaded from inline root (if present)
     // otherwise from cwd
     for (const filename of DEFAULT_CONFIG_FILES) {
-      const filePath = path.resolve(configRoot, filename)
-      if (!fs.existsSync(filePath)) continue
+      const filePath = path.resolve(configRoot, filename);
+      if (!fs.existsSync(filePath)) continue;
 
-      resolvedPath = filePath
-      break
+      resolvedPath = filePath;
+      break;
     }
   }
 
   if (!resolvedPath) {
-    console.error('no config file found.')
-    return null
+    console.error('no config file found.');
+    return null;
   }
 
-  let isESM = false
+  let isESM = false;
   if (/\.m[jt]s$/.test(resolvedPath)) {
-    isESM = true
+    isESM = true;
   } else if (/\.c[jt]s$/.test(resolvedPath)) {
-    isESM = false
+    isESM = false;
   } else {
     // check package.json for type: "module" and set `isESM` to true
     try {
-      const pkg = lookupFile(configRoot, ['package.json'])
-      isESM = !!pkg && JSON.parse(pkg).type === 'module'
+      const pkg = lookupFile(configRoot, ['package.json']);
+      isESM = !!pkg && JSON.parse(pkg).type === 'module';
     } catch (e) {}
   }
 
   try {
-    const bundled = await bundleConfigFile(resolvedPath, isESM)
+    const bundled = await bundleConfigFile(resolvedPath, isESM);
     const userConfig = await loadConfigFromBundledFile(
       resolvedPath,
       bundled.code,
       isESM
-    )
-    console.log(`bundled config file loaded in ${getTime()}`)
+    );
+    console.log(`bundled config file loaded in ${getTime()}`);
 
     const config = userConfig;
     if (!isObject(config)) {
-      throw new Error(`config must export or return an object.`)
+      throw new Error(`config must export or return an object.`);
     }
     return {
       path: normalizePath(resolvedPath),
       config,
       dependencies: bundled.dependencies
-    }
+    };
   } catch (e) {
-    throw e
+    throw e;
   }
 }
 
@@ -158,20 +158,20 @@ async function bundleConfigFile(
         name: 'externalize-deps',
         setup(build) {
           build.onResolve({ filter: /.*/ }, (args) => {
-            const id = args.path
+            const id = args.path;
             if (id[0] !== '.' && !path.isAbsolute(id)) {
               return {
                 external: true
-              }
+              };
             }
-          })
+          });
         }
       },
       {
         name: 'replace-import-meta',
         setup(build) {
           build.onLoad({ filter: /\.[jt]s$/ }, async (args) => {
-            const contents = await fs.promises.readFile(args.path, 'utf8')
+            const contents = await fs.promises.readFile(args.path, 'utf8');
             return {
               loader: args.path.endsWith('.ts') ? 'ts' : 'js',
               contents: contents
@@ -184,23 +184,25 @@ async function bundleConfigFile(
                   JSON.stringify(path.dirname(args.path))
                 )
                 .replace(/\b__filename\b/g, JSON.stringify(args.path))
-            }
-          })
+            };
+          });
         }
       }
     ]
-  })
-  const { text } = result.outputFiles[0]
+  });
+  const { text } = result.outputFiles[0];
   return {
     code: text,
     dependencies: result.metafile ? Object.keys(result.metafile.inputs) : []
-  }
+  };
 }
 
 interface NodeModuleWithCompile extends NodeModule {
-  _compile(code: string, filename: string): any
+  _compile(code: string, filename: string): any;
 }
-const _require = createRequire(import.meta.url)
+
+const _require = createRequire(import.meta.url);
+
 async function loadConfigFromBundledFile(
   fileName: string,
   bundledCode: string,
@@ -210,15 +212,15 @@ async function loadConfigFromBundledFile(
   // with --experimental-loader themselves, we have to do a hack here:
   // write it to disk, load it with native Node ESM, then delete the file.
   if (isESM) {
-    const fileBase = `${fileName}.timestamp-${Date.now()}`
-    const fileNameTmp = `${fileBase}.mjs`
-    const fileUrl = `${pathToFileURL(fileBase)}.mjs`
-    fs.writeFileSync(fileNameTmp, bundledCode)
+    const fileBase = `${fileName}.timestamp-${Date.now()}`;
+    const fileNameTmp = `${fileBase}.mjs`;
+    const fileUrl = `${pathToFileURL(fileBase)}.mjs`;
+    fs.writeFileSync(fileNameTmp, bundledCode);
     try {
-      return (await import(fileUrl)).default
+      return (await import(fileUrl)).default;
     } finally {
       try {
-        fs.unlinkSync(fileNameTmp)
+        fs.unlinkSync(fileNameTmp);
       } catch {
         console.error('some error here');
         // already removed if this function is called twice simultaneously
@@ -227,21 +229,21 @@ async function loadConfigFromBundledFile(
   }
   // for cjs, we can register a custom loader via `_require.extensions`
   else {
-    const extension = path.extname(fileName)
-    const realFileName = fs.realpathSync(fileName)
-    const loaderExt = extension in _require.extensions ? extension : '.js'
-    const defaultLoader = _require.extensions[loaderExt]!
+    const extension = path.extname(fileName);
+    const realFileName = fs.realpathSync(fileName);
+    const loaderExt = extension in _require.extensions ? extension : '.js';
+    const defaultLoader = _require.extensions[loaderExt]!;
     _require.extensions[loaderExt] = (module: NodeModule, filename: string) => {
       if (filename === realFileName) {
-        ;(module as NodeModuleWithCompile)._compile(bundledCode, filename)
+        ;(module as NodeModuleWithCompile)._compile(bundledCode, filename);
       } else {
-        defaultLoader(module, filename)
+        defaultLoader(module, filename);
       }
-    }
+    };
     // clear cache in case of server restart
-    delete _require.cache[_require.resolve(fileName)]
-    const raw = _require(fileName)
-    _require.extensions[loaderExt] = defaultLoader
-    return raw.__esModule ? raw.default : raw
+    delete _require.cache[_require.resolve(fileName)];
+    const raw = _require(fileName);
+    _require.extensions[loaderExt] = defaultLoader;
+    return raw.__esModule ? raw.default : raw;
   }
 }
